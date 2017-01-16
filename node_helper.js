834645bf1a6559c951e91765686bec5cfea62a0a
in-a-days <john@battlebornmusic.com>
2017-01-16 15:23:27 -0800

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -11,59 +11,26 @@ const parser = require('xml2js').parseString;
 const moment = require('moment-timezone');
 const StatisticsAPI = require("./StatisticsAPI.js");
 const NodeHelper = require("node_helper");
+const scores
 
 module.exports = NodeHelper.create({
 
-    scores: [],
-    details: {},
-    nextMatch: null,
-    live: {
-        state: false,
-        matches: []
+    python_start: function () {
+        const self = this;
+        const pyshell = new PythonShell('modules/' + this.name + '/MMM-NBA/MMM-NBA.py', { mode: 'json', args: [JSON.stringify(this.config)]});
     },
-
-    start: function() {
-        console.log("Starting module: " + this.name);
-    },
-
-    socketNotificationReceived: function(notification, payload) {
-        if(notification === 'CONFIG'){
-            this.config = payload;
-            this.getData();
-            setInterval(() => {
-                this.getData();
-            }, this.config.reloadInterval);
-            setInterval(() => {
-                this.fetchOnLiveState();
-            }, 60*1000);
-        }
-    },
-
-    getData: function() {
-        request({url: this.urls[this.mode]}, (error, response, body) => {
-            if (response.statusCode === 200) {
-                parser(body, (err, result) => {
-                    if(err) {
-                        console.log(err);
-                    } else if(result.hasOwnProperty('ss')){
-                        this.scores = result.ss.gms[0].g;
-                        this.details = result.ss.gms[0].$;
-                        this.setMode();
-                        this.sendSocketNotification("SCORES", {scores: this.scores, details: this.details});
-                        return;
-                    } else {
-                        console.log("Error no NFL data");
-                    }
-                });
-            } else {
-                console.log("Error getting NFL scores " + response.statusCode);
-            }
-        });
-    },
-
-    fetchOnLiveState: function(){
-        if(this.live.state === true){
-            this.getData();
-        }
+    
+  // Subclass socketNotificationReceived received.
+  socketNotificationReceived: function(notification, payload) {
+    if(notification === 'CONFIG') {
+      this.config = payload
+      if(!pythonStarted) {
+        pythonStarted = true;
+        this.python_start();
+        };
     }
+    else if(notification === 'NBASCORES') {
+        this.sendSocketNotification("SCORES", scores);     
+  }
+
 });
