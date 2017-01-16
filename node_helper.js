ed1e0af29daad7afccf05769f43b4528de2e6db3
in-a-days <john@battlebornmusic.com>
2017-01-16 14:48:28 -0800

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -61,66 +61,6 @@ module.exports = NodeHelper.create({
         });
     },
 
-    setMode: function(){
-        var all_ended = true;
-        var next = null;
-        var now = Date.now();
-        var in_game = ['1', '2', '3', '4', 'H', 'OT'];
-        var ended = ['F', 'FO', 'T'];
-        for(var i = 0; i < this.scores.length; i++) {
-            var temp = this.scores[i].$;
-            this.scores[i].$.starttime = moment.tz(
-                temp.eid.slice(0, 4) + "-" + temp.eid.slice(4, 6) + "-" + temp.eid.slice(6, 8) + " " + ("0" + (12 + parseInt(temp.t.split(':')[0])) + temp.t.slice(-3)).slice(-5),
-                "America/New_York"
-            );
-            if(this.scores[i].$.q === "P"){
-                all_ended = false;
-                if(next === null){
-                    next = this.scores[i].$;
-                }
-            } else if((in_game.indexOf(this.scores[i].$.q) !== -1 || Date.parse(this.scores[i].$.starttime) > now) && this.live.matches.indexOf(this.scores[i].$.gsis) === -1){
-                all_ended = false;
-                this.live.matches.push(this.scores[i].$.gsis);
-                this.live.state = true;
-            } else if(ended.indexOf(this.scores[i].$.q) !== -1 && (index = this.live.matches.indexOf(this.scores[i].$.gsis)) !== -1){
-                this.live.matches.splice(index, 1);
-                if(this.live.matches.length === 0){
-                    this.live.state = false;
-                }
-            }
-        }
-
-        var current_date = new Date();
-        if(this.mode === "regular" && this.details.w >= 17 && (current_date.getMonth() < 1 || current_date.getMonth() > 10) && all_ended){
-            this.mode = "post";
-            this.getData();
-            return;
-        } else if(this.mode === "post" && current_date.getMonth() >= 5){
-            this.mode = "regular";
-            this.getData();
-            return;
-        }
-
-        for(var i = this.scores.length - 2; i >= 0; i--){
-            var previous = this.scores[i].$.starttime;
-            var match = this.scores[i + 1].$.starttime;
-            if(previous.diff(match) > 0){
-                previous.subtract(12, 'hours');
-            }
-        }
-
-        if(all_ended === true){
-            this.nextMatch = null;
-        }
-
-        if(this.nextMatch === null && all_ended === false || this.live.state === true){
-            this.nextMatch = {
-                id: next.gsis,
-                time: next.starttime
-            }
-        }
-    },
-
     fetchOnLiveState: function(){
         if(this.live.state === true){
             this.getData();
